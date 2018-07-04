f3a56bba9384c44136d0ad42efb19b6decc88433
abhimangalms <abhimangalms@gmail.com>
2018-07-04 18:12:31 +0530

    filter search added

--- app/src/main/java/com/audreytechnologies/mclinic/MainActivity.java
+++ app/src/main/java/com/audreytechnologies/mclinic/MainActivity.java
@@ -1,5 +1,6 @@
 package com.audreytechnologies.mclinic;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -51,8 +52,11 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+
+                Intent intent = new Intent(getApplicationContext(),UserProfile.class);
+                startActivity(intent);
             }
         });
 
